17c116345ef0f1cd9fa87951ae3f59e7bb8e4e09
M.Thomas <m.thomas@gso.schule.koeln>
2025-03-24 13:25:02 +0100

    Tests erfolgreich

--- Reisekosten/Uebung/Reisekosten.cs
+++ Reisekosten/Uebung/Reisekosten.cs
@@ -4,7 +4,34 @@ namespace Uebung
     {
         public static double BerechneErstattung(int kilometer)
         {
-           throw new NotImplementedException();
+            if (kilometer < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+            int letzteStelle = kilometer % 10;
+
+            if (letzteStelle > 0)
+            {
+                kilometer = kilometer - letzteStelle;
+                kilometer = kilometer + 10;
+            }
+            
+            if (kilometer > 1000)
+            {
+                kilometer = 1000;
+            }
+
+            int volleErstattungKilometer = kilometer;
+            int reduzierteErstattungKilometer = 0;
+            
+            if (kilometer > 500)
+            {
+                volleErstattungKilometer = 500;
+                reduzierteErstattungKilometer = kilometer - volleErstattungKilometer;
+            }
+
+            return volleErstattungKilometer * 0.3 + reduzierteErstattungKilometer * 0.25;
         }
         
     }
